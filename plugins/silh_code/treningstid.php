1f96bb125fb6ba2c1c55678a7a30d0e3fe481d9e
Otto Paulsen <otto.paulsen@itema.no>
2014-12-09 22:21:14 +0100

    Sortert tabell med treningstider

--- plugins/silh_code/treningstid.php
+++ plugins/silh_code/treningstid.php
@@ -9,21 +9,7 @@
 /* Place custom code below this line. */
 
 
-// Vis alle treningstider registrert med Formidable   
-add_shortcode( 'LagetsTreningstider', 'lagetsTreningstider_func' );
-function lagetsTreningstider_func($atts){
-	$lag_id = get_post_meta( get_the_ID(), 'lag_id', true );
-	if ( $lag_id ) :
-	  $ret = FrmProDisplaysController::get_shortcode(array('id' => 866, 'lag_id' => $lag_id));
-	else :
-	  $ret = '<h3>Treningstider</h3>';
-	  $ret .= 'Denne modulen heter "Treningstider" og kan brukes for å vise lagets treningstider.';
-	  $ret .= '<br>Siden mangler det tilpassede feltet "lag_id", som må til for at modulen skal virke.';
-	  $ret .= '<br>Kontakt webmaster om du ikke vet hvordan du skal fikse det selv.';
-	endif;
-
-	return $ret;
-}   
+ 
 
 add_shortcode( 'RegistrerTreningstid', 'registrerTreningstid_func' );
 function registrerTreningstid_func($atts){
@@ -38,6 +24,72 @@ function registrerTreningstid_func($atts){
 
 	return $res;
 }
+
+
+// Vis alle treningstider registrert med Formidable   
+add_shortcode( 'LagetsTreningstider', 'treningstider_func' );
+add_shortcode( 'treningstider', 'treningstider_func' );
+function treningstider_func($atts){
+
+    global $frm_entry, $frm_entry_meta;
+
+	$lag_id = get_post_meta( get_the_ID(), 'lag_id', true );
+
+    $entries = $frm_entry->getAll("it.form_id=13");
+
+    $dayOrder = array('Mandag'=>1,
+    	              'Tirsdag'=>2,
+    	              'Onsdag'=>3,
+    	              'Torsdag'=>4,
+    	              'Fredag'=>5,
+    	              'Lørdag'=>6,
+    	              'Søndag'=>7);
+
+    $res = '<table class="silhTable">
+            <caption style="caption-side:bottom;text-align:left;border:none;background:none;margin:0;padding:0;"></caption>
+            <thead>
+            <tr>
+            <th class="column-1"><div>Dag</div></th>
+            <th class="column-2"><div>Sted</div></th>
+            <th class="column-3"><div>Tid</div></th>';
+
+    if(!$lag_id) $res .= '<th class="column-4"><div>Lag</div></th>';
+
+    $res .= '</tr></thead><tbody>';
+
+    foreach ( $entries as $entry ) {
+        $lag = $frm_entry_meta->get_entry_meta_by_field($entry->id, 125, true);
+        if (!$lag_id or $lag == $lag_id) {
+	        $dag = $frm_entry_meta->get_entry_meta_by_field($entry->id, 120, true);
+	        $sted = $frm_entry_meta->get_entry_meta_by_field($entry->id, 112, true);
+	        $tid = $frm_entry_meta->get_entry_meta_by_field($entry->id, 111, true);
+
+	        $sort = $dayOrder[$dag] . $sted . $tid . $lag;
+	        $sorter[] = $sort;
+	        $tider[] = array('dag' => $dag, 'sted' => $sted, 'tid' => $tid, 'lag' => $lag);
+	    }
+    }
+
+    array_multisort($tider, SORT_NATURAL, $sorter);
+
+    $odd = true;
+    foreach ($tider as $t) {
+        $res .= '<tr class="' . ($odd ? 'odd' : 'even') . '"><td>' . 
+               $t['dag'] . '</td><td>' . 
+               $t['sted'] . '</td><td>' . 
+               $t['tid'] . '</td>';
+        if(!$lag_id) $res .= '<td>' . $t['lag'] . '</td>';
+        $res .= '</tr>';
+
+        $odd = !$odd;
+    }
+
+
+    $res .= '</tbody></table>';
+    return $res;
+}
+
+
 
 /* Place custom code above this line. */
 ?>
